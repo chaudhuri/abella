c1f63bcbc11a1300b21b88e979e025da603baa7a
Kaustuv Chaudhuri <kaustuv.chaudhuri@inria.fr>
2024-12-04 09:39:13 +0100

    wip: fix equality goals to handle equivs

--- src/tactics.ml
+++ src/tactics.ml
@@ -1099,9 +1099,15 @@ let search ~depth:n ~hyps ~clauses ~def_unfold ~sr ~retype
     | Eq(left, right) -> begin
         match witness with
         | WMagic | WReflexive ->
-            unwind_state
-              (fun () -> if try_right_unify left right then sc WReflexive)
-              ()
+            unwind_state begin fun () ->
+              match try_right_unify_cpairs_fully_inferred ~msg:"equality-right" left right with
+              | None | Some { cpairs = [] ; equivs = [] } -> sc WReflexive
+              | Some { cpairs = [] ; equivs } ->
+                  let goal = List.map (fun q -> Pred (q, Irrelevant)) equivs
+                             |> conjoin in
+                  metaterm_aux n hyps goal ts ~sc:(fun w -> sc (WLeft w)) ~witness
+              | _ -> ()
+            end ()
         | _ -> bad_witness ()
       end
     | Or(left, right) -> begin
